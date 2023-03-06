a85b28cdee186269d4f67c53c95690bc6be8d194
MD Basiir <muhdbasiir@gmail.com>
2023-03-06 16:17:24 +0800

    route login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\Http\Controllers\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AutheticatesUsers;
+use Auth;
+
+class LoginController extends Controller {
+
+  use AuthenticatesUsers;
+
+    protected $redirectTo;
+
+    public function redirectTo() {
+      switch (Auth::user()->role){
+        case 'admin':
+          $this->redirectTo = route('admin.dashboard');
+          break;
+        case 'user':
+          $this->redirectTo = route('users.index');
+          break;
+        default:
+          $this->redirectTo = route('login');
+          return $this->redirectTo;
+      }
+    }
+  }
