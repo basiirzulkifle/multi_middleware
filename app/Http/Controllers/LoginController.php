737b2edc5def5c96f06357a685cbc69fd2c797bb
MD Basiir <muhdbasiir@gmail.com>
2023-03-06 16:19:01 +0800

    test

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,27 +1,31 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AutheticatesUsers;
 use Auth;
 
-class LoginController extends Controller {
+class LoginController extends Controller
+{
 
   use AuthenticatesUsers;
+  //testing lazygit
 
-    protected $redirectTo;
+  protected $redirectTo;
 
-    public function redirectTo() {
-      switch (Auth::user()->role){
-        case 'admin':
-          $this->redirectTo = route('admin.dashboard');
-          break;
-        case 'user':
-          $this->redirectTo = route('users.index');
-          break;
-        default:
-          $this->redirectTo = route('login');
-          return $this->redirectTo;
-      }
+  public function redirectTo()
+  {
+    switch (Auth::user()->role) {
+      case 'admin':
+        $this->redirectTo = route('admin.dashboard');
+        break;
+      case 'user':
+        $this->redirectTo = route('users.index');
+        break;
+      default:
+        $this->redirectTo = route('login');
+        return $this->redirectTo;
     }
   }
+}
